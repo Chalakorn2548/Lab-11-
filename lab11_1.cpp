a0b8bd34ebca03fc3e1f0e1ae4ef1107ceb6ad1e
Chalakorn <chalakorn.nk@gmail.com>
2025-12-30 11:31:10 +0700

    Update lab11_1.cpp
    edit code

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "Press Enter 3 times to reveal your future.";
+    
+    // รอรับการกด Enter 3 ครั้ง
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    // ตั้งค่า Seed เพื่อให้การสุ่มเปลี่ยนไปตามเวลา (ถ้าไม่ใส่จะได้ค่าเดิมตลอด)
+    srand(time(0));
+    
+    // สุ่มตัวเลข 0 ถึง 8 (เพราะมีเกรด 9 ตัว: A, B+, B, C+, C, D+, D, F, W)
+    int x = rand() % 9;
+    
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    
+    // แสดงผลคำทำนาย
+    cout << "You will get " << grades[x] << " in this 261102.";
+    
+    return 0;
+}
